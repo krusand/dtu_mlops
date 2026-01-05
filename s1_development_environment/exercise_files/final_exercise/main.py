65eabce8f06e8642c0f10c583c7e20deb3c0f980
krusand <110490348+krusand@users.noreply.github.com>
2026-01-05 21:08:58 +0100

    day 1 exercises

--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -1,7 +1,15 @@
 import torch
+from torch import nn
+
 import typer
-from data_solution import corrupt_mnist
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from data import corrupt_mnist
 from model import MyAwesomeModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 app = typer.Typer()
 
@@ -13,8 +21,37 @@ def train(lr: float = 1e-3) -> None:
     print(lr)
 
     # TODO: Implement training loop here
-    model = MyAwesomeModel()
+    model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=32)
+
+    criterion = nn.NLLLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.005)
+
+    epochs = 15
+    steps = 0
+
+    train_losses = []
+    for _ in tqdm(range(epochs)):
+        model.train()
+        batch_loss = 0
+        for images, labels in train_dataloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            optimizer.zero_grad()
+
+            log_ps = model(images)
+            loss = criterion(log_ps, labels)
+            loss.backward()
+            optimizer.step()
+            batch_loss += loss.item()
+
+        train_losses.append(batch_loss)
+
+    torch.save(model.state_dict(), "models/s1_model.pt")
+
+    plt.plot(range(0,epochs),train_losses)
+    plt.title("Training loss")
+    plt.show()
 
 
 @app.command()
@@ -24,8 +61,28 @@ def evaluate(model_checkpoint: str) -> None:
     print(model_checkpoint)
 
     # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
+    model = MyAwesomeModel().to(DEVICE)
+    model.load_state_dict(torch.load(model_checkpoint))
+    model.eval()
     _, test_set = corrupt_mnist()
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=32)
+
+    with torch.no_grad():
+        batch_test_loss = 0
+        batch_accuracy = []
+        for images, labels in test_dataloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            log_ps = model(images)
+            ps = torch.exp(log_ps)
+
+            top_p, top_class = ps.topk(k=1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+
+            accuracy = torch.mean(equals.type(torch.FloatTensor))
+            batch_accuracy.append(accuracy.item())
+        accuracy = torch.mean(torch.Tensor(batch_accuracy))
+        print(100*accuracy.item(), "%")
+
 
 
 if __name__ == "__main__":
--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torch
+from torch.nn.functional import log_softmax, relu
 
 
 class MyAwesomeModel(nn.Module):
@@ -6,4 +8,29 @@ class MyAwesomeModel(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
-        self.fc1 = nn.Linear(784, 128)
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        """Forward pass through the network, returns the output logits."""
+        # make sure input tensor is flattened
+        x = x.view(x.shape[0], -1)
+
+        x = self.dropout(relu(self.fc1(x)))
+        x = self.dropout(relu(self.fc2(x)))
+        x = self.dropout(relu(self.fc3(x)))
+        return log_softmax(self.fc4(x), dim=1)
+    
+
+if __name__ == '__main__':
+    model = MyAwesomeModel()
+    print(model)
+
+    dummy_input = torch.randn(1, 1, 28, 28)
+    output = model(dummy_input)
+    print(f"Output shape: {output.shape}")
+    print(f"Output: {output}")
